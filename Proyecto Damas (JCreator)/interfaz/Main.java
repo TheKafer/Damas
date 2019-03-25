c4ada5f1596e540594efd3b58f706561cce0962a
TheKafer <38733280+TheKafer@users.noreply.github.com>
2019-03-25 11:54:58 -0500

    a
    a

---
+++ Proyecto Damas (JCreator)/interfaz/Main.java
@@ -0,0 +1,12 @@
+package interfaz;
+import interfaz.PrincipalJuego;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        PrincipalJuego pj = new PrincipalJuego();
+        
+    }
+
+}
